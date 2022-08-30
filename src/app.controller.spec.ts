03a6df4cabae727336d7b2ee0a229c64d08c7af5
Luccas Adriano <devluccas@DESKTOP-3C6SL42.localdomain>
2022-08-30 18:01:18 -0300

    test: update test controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { KafkaModule } from './kafka/kafka.module'
+import { ProducerService } from './kafka/producer.service'
+import { TestConsumer } from './test.consumer'
 
 describe('AppController', () => {
   let appController: AppController
 
+  const producerService = {
+    produce: jest.fn().mockResolvedValue(
+      Promise.resolve({
+        topic: 'test',
+        messages: [
+          {
+            value: 'Hello World!',
+          },
+        ],
+      }),
+    ),
+  }
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [KafkaModule],
       controllers: [AppController],
-      providers: [AppService],
-    }).compile()
+      providers: [AppService, TestConsumer],
+    })
+      .overrideProvider(ProducerService)
+      .useValue(producerService)
+      .compile()
 
     appController = app.get<AppController>(AppController)
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('should return "Hello World!"', async () => {
+      expect(await appController.getHello()).toBe('Hello World!')
     })
   })
 })
